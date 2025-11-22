76829f421483bd948e8ec1fb742ea897a5f7d8a8
Florence Shen <fshen0426@gmail.com>
2025-11-22 15:48:02 -0800

    Initial Commit

---
+++ CodingKeyboard/CodingKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingKeyboardApp.swift
+//  CodingKeyboard
+//
+//  Created by Florence on 2025-11-22.
+//
+
+import SwiftUI
+
+@main
+struct CodingKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
