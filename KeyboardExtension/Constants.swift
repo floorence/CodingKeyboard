d4ca1de6ab5b2e44282c14773520fb3b9f7e66c9
Florence Shen <fshen0426@gmail.com>
2025-12-13 21:36:18 -0800

    use constants, fix colours, shift key, different keys widths

--- KeyboardExtension/Constants.swift
+++ KeyboardExtension/Constants.swift
@@ -5,13 +5,28 @@
 //  Created by Florence on 2025-11-29.
 //
 
-class Constants {
-    static let keysPage1 = [
+import UIKit
+
+class Keys {
+    static let KEYS_1 = [
         [",", ".", "/", "\\", "-", "=", "[", "]", ";", "\'"],
         ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
         ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
         ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
         ["⇧", "z", "x", "c", "v", "b", "n", "m", "⌫"],
-        ["→", "🌐", "space", "return"]
+        ["→", "🌐", " ", "return"]
     ]
+    static let SQUARE_KEYS = ["⇧", "⌫", "→", "🌐"]
+    static let WIDE_KEYS = ["return"]
+}
+
+class Dimens {
+    static let KEY_HEIGHT: CGFloat = 50
+    static let KEY_WIDE_WIDTH: CGFloat = 80
+    static let KEY_CORNER_RADIUS: CGFloat = 8
+    static let KEYS_TOP_MARGIN: CGFloat = 12
+    static let KEYS_SPACING: CGFloat = 5
+    static let EDGE_MARGIN: CGFloat = 5
+    static let KEY_FONT_SIZE: CGFloat = 24
+    static let KEYBOARD_HEIGHT: CGFloat = CGFloat(Keys.KEYS_1.count) * (KEY_HEIGHT + KEYS_TOP_MARGIN)
 }
--- KeyboardExtension/KeyboardViewController.swift
+++ KeyboardExtension/KeyboardViewController.swift
@@ -22,10 +22,20 @@ class KeyboardViewController: UIInputViewController {
     var shiftState: ShiftState = .none
     var keysPage: Int = 1
     
+    var keyWidth: CGFloat = 0
+    var extraMargin: CGFloat = 0
+    var extraSpacing: CGFloat = 0
+    var spaceBarWidth: CGFloat = 0
+    
     override func updateViewConstraints() {
         super.updateViewConstraints()
         
-        // Add custom view sizing constraints here
+        if view.constraints.contains(where: { $0.identifier == "keyboardHeight" }) { return }
+        
+        let heightConstraint = view.heightAnchor.constraint(equalToConstant: Dimens.KEYBOARD_HEIGHT)
+        heightConstraint.identifier = "keyboardHeight"
+        heightConstraint.isActive = true
+//        view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
     
     // TODO: investigate -[UIInputViewController needsInputModeSwitchKey] was called before a connection was established to the host application. This will produce an inaccurate result. Please make sure to call this after your primary view controller has been initialized.
@@ -47,78 +57,152 @@ class KeyboardViewController: UIInputViewController {
         self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
         self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
-        view.backgroundColor = .systemGray
-        
         initRows()
+    }
+   
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        calculateDynamicDimens()
         drawKeys()
     }
     
+    func calculateDynamicDimens() {
+        print("calculateDynamicDimens()")
+        let keysInRow = CGFloat(Keys.KEYS_1[0].count)
+        let whiteSpaceWidth = (Dimens.EDGE_MARGIN * 2) + (Dimens.KEYS_SPACING * (keysInRow - 1))
+        keyWidth = (view.bounds.width - whiteSpaceWidth) / keysInRow
+        print(keyWidth)
+        
+        let keyWidthWithSpacing = keyWidth + Dimens.KEYS_SPACING
+        extraMargin = keyWidthWithSpacing / 2
+        print(extraMargin)
+        
+        let secondLastRowKeysWidth = (keyWidth * 7) + (Dimens.KEY_HEIGHT * 2)
+        let secondLastRowWhiteSpaceWidth = (Dimens.EDGE_MARGIN * 2) + (Dimens.KEYS_SPACING * 8)
+        extraSpacing = (view.bounds.width - secondLastRowKeysWidth - secondLastRowWhiteSpaceWidth) / 2
+        print(extraSpacing)
+        
+        let lastRowKeysWidth = keyWidth * (needsInputModeSwitchKey ? 2 : 1) + Dimens.KEY_WIDE_WIDTH
+        let lastRowWhiteSpaceWidth = (Dimens.EDGE_MARGIN * 2) + (Dimens.KEYS_SPACING * 3)
+        spaceBarWidth = view.bounds.width - lastRowKeysWidth - lastRowWhiteSpaceWidth
+        print(spaceBarWidth)
+    }
+ 
     func initRows() {
-        for i in 0...Constants.keysPage1.count - 1 {
+        for i in 0...Keys.KEYS_1.count - 1 {
             let row = UIStackView()
             row.axis = .horizontal
-            row.distribution = .fillEqually
-            row.spacing = 6
+            row.distribution = .fill
+//            row.spacing = Dimens.KEYS_SPACING
             row.translatesAutoresizingMaskIntoConstraints = false
             
             rows.append(row)
             view.addSubview(row)
             
             var topAnchor: NSLayoutYAxisAnchor
+            var topMargin: CGFloat
             if (i == 0) {
                 topAnchor = view.topAnchor
+                topMargin = Dimens.EDGE_MARGIN
             } else {
                 topAnchor = rows[i - 1].bottomAnchor
+                topMargin = Dimens.KEYS_TOP_MARGIN
             }
             
             NSLayoutConstraint.activate([
-                row.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
-                row.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
-                row.topAnchor.constraint(equalTo: topAnchor, constant: 15),
-                row.heightAnchor.constraint(equalToConstant: 40)
+                row.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Dimens.EDGE_MARGIN),
+                row.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Dimens.EDGE_MARGIN),
+                row.topAnchor.constraint(equalTo: topAnchor, constant: topMargin),
+                row.heightAnchor.constraint(equalToConstant: Dimens.KEY_HEIGHT)
             ])
         }
     }
     
     func drawKeys() {
-        for row in 0...Constants.keysPage1.count - 1 {
-            for view in rows[row].arrangedSubviews {
-                view.removeFromSuperview()
-            }
-            for col in 0...Constants.keysPage1[row].count - 1 {
-                let symbol = Constants.keysPage1[row][col]
+        for row in 0...Keys.KEYS_1.count - 1 {
+            removeAllSubViews(rows[row])
+            if (row == 3) { rows[row].addArrangedSubview(spacer(extraMargin)) }
+            for col in 0...Keys.KEYS_1[row].count - 1 {
+                let symbol = Keys.KEYS_1[row][col]
                 let button = createKey(title: symbol)
                 rows[row].addArrangedSubview(button)
+                
+                if (col != Keys.KEYS_1[row].count - 1) {
+                    rows[row].addArrangedSubview(spacer(Dimens.KEYS_SPACING))
+                }
+               
+                if (symbol == "⇧" || symbol == "m") {
+                    rows[row].addArrangedSubview(spacer(extraSpacing))
+                }
             }
+            if (row == 3) { rows[row].addArrangedSubview(spacer(extraMargin)) }
         }
     }
     
     func createKey(title: String) -> UIButton {
         let button = UIButton(type: .system)
-        button.setTitle(title, for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 22)
-        button.titleLabel?.textColor = .black
         
-        button.backgroundColor = .white
-        button.layer.setValue(title, forKey: "key")
-        button.layer.cornerRadius = 8
-        button.layer.borderColor = UIColor.systemGray4.cgColor
-        button.layer.shadowColor = UIColor.systemGray4.cgColor
-        button.layer.shadowRadius = 1
+        let capsKey = title.capitalized
+        let displayKey = shiftState == .none ? title : capsKey
+        button.setTitle(displayKey, for: .normal)
+        button.layer.setValue(displayKey, forKey: "key")
+        
+        button.titleLabel?.font = .systemFont(ofSize: Dimens.KEY_FONT_SIZE)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.backgroundColor = UIColor.systemBackground
+        button.layer.cornerRadius = Dimens.KEY_CORNER_RADIUS
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        if (Keys.SQUARE_KEYS.contains(title)) {
+            button.widthAnchor.constraint(equalToConstant: Dimens.KEY_HEIGHT).isActive = true
+        } else if (Keys.WIDE_KEYS.contains(title)) {
+            button.widthAnchor.constraint(equalToConstant: Dimens.KEY_WIDE_WIDTH).isActive = true
+        } else if (title == " ") {
+            button.widthAnchor.constraint(equalToConstant: spaceBarWidth).isActive = true
+        } else {
+            button.widthAnchor.constraint(equalToConstant: keyWidth).isActive = true
+        }
         
         button.addTarget(self, action: #selector(keyPressed(_:)), for: .touchUpInside)
         return button
+    }
+    
+   
+    func removeAllSubViews(_ stackView: UIStackView) {
+        for view in stackView.arrangedSubviews {
+            view.removeFromSuperview()
+        }
+    }
+    
+    func spacer(_ width: CGFloat) -> UIView {
+        let view = UIView()
+        view.widthAnchor.constraint(equalToConstant: width).isActive = true
+        return view
+    }
+    
+    func unShiftAndInvalidate() {
+        if (shiftState == .shift) {
+            shiftState = .none
+            drawKeys()
+        }
     }
     
     @objc func keyPressed(_ sender: UIButton) {
         guard let key = sender.layer.value(forKey: "key") as? String else {return}
         
         switch key {
-        case "⇧": shiftState = (shiftState == .none) ? .shift : .none
+        case "⌫":
+            unShiftAndInvalidate()
+            textDocumentProxy.deleteBackward()
+        case "→":
+            textDocumentProxy.insertText("\t")
+        case "return":
+            textDocumentProxy.insertText("\n")
+        case "⇧":
+            shiftState = (shiftState == .none) ? .shift : .none
+            drawKeys()
         default:
-            if (shiftState == .shift) {
-                shiftState = .none
-            }
+            unShiftAndInvalidate()
             textDocumentProxy.insertText(key)
         }
     }
